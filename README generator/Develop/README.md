a17da0fba692440d30948ff432663ddd44affc53
Alex <tatum_alex@ymail.com>
2020-11-10 14:51:55 -0500

    first commit

---
+++ README generator/Develop/README.md
@@ -0,0 +1,26 @@
+# README Generator
+
+  ## Description
+  Creates professional README files quickly.
+  ## License Badge
+  ![License badge](https://img.shields.io/badge/license-MIT-green)
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contribution](#Contribution)
+  5. [Questions](#Questions)
+  ## Installation
+  Open terminal. Find correct directory. Run "npm install" command. Run "node index.js" command. Create README.md file by answering each prompt.
+  ## Usage
+  Use this to quickly create professional README files.
+  ## License
+  * MIT
+  * ![License badge](https://img.shields.io/badge/license-MIT-green)
+  ## Contribution
+  undefined
+  ## Tests
+  N/A
+  ## Questions
+  * My GitHub profile can be viewed here: https://github.com/Alex-Tatum
+  * You can email me with questions at: tatum_alex@ymail.com
---
+++ README generator/Develop/utils/generateMarkdown.js
@@ -0,0 +1,31 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Description
+  ${data.description}
+  ## License Badge
+  ![License badge](https://img.shields.io/badge/license-${data.license}-green)
+  ## Table of Contents
+  1. [Installation](#Installation)
+  2. [Usage](#Usage)
+  3. [License](#License)
+  4. [Contribution](#Contribution)
+  5. [Questions](#Questions)
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## License
+  * ${data.license}
+  * ![License badge](https://img.shields.io/badge/license-${data.license}-green)
+  ## Contribution
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  * My GitHub profile can be viewed here: https://github.com/${data.username}
+  * You can email me with questions at: ${data.email}`;
+}
+
+module.exports = generateMarkdown;
